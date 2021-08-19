fd6a68888374c1052a1062a603e645e9134d4e89
B3P <39571509+Wikipediano@users.noreply.github.com>
2021-08-18 17:16:37 -0700

    bullshit

--- mastermind.c
+++ mastermind.c
@@ -2,6 +2,20 @@
 
 #define patternSize 8
 
+int generatePattern()
+{
+    int pattern[patternSize];
+    for (int i = 0; i < patternSize; i++)
+    {
+        pattern[i] = i + 1;
+    }
+    for (int i = patternSize - 1; i > 0; i--)
+    {
+        int j =
+    }
+    return pattern;
+}
+
 int main()
 {
     return 0;
